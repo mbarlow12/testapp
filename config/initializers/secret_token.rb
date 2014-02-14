994b4ba67e002a8df64d144edfdf86d16356a790
Michael Barlow <m.barlow12@gmail.com>
2014-02-13 18:09:32 -0700

    Added bootstrap and basic status page styling and layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '9968e018714fb37c82bde3882c58d00b3a5ed7ac78dbb0eea5ed668213b5664170e87423618f6a84987ba60af8c73a69f6f425810dd4d3bb167078c653a0a8e3'
